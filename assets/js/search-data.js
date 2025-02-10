8aeb704afcab791624140ea2e0cb9399c11fe523
QingyuanYuu <jaaasn_yu@outlook.com>
2025-02-10 04:54:30 +0000

    Deploying to gh-pages from @ QingyuanYuu/QingyuanYuu.github.io@11997874d441012898db0f90552606285f6703e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,18 +72,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-advanced-image-components",
-      
-        title: "a post with advanced image components",
-      
-      description: "this is what advanced image components could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/advanced-images/";
-        
-      },
-    },{id: "post-a-post-with-twitter",
+            },{id: "post-a-post-with-twitter",
       
         title: "a post with twitter",
       
